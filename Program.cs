4319a25a2d1b12d10a82d62773040468f360f385
kwiennyu <dalumpinesmichael6@gmail.com>
2024-08-26 12:13:37 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DataTypes;
+
+class DataTypesProgram
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Name: Quenie Fritz Indencio ");
+        Console.Write("Section: BSCS2-1AA ");
+
+    Console.WriteLine("\nEnter the pieces of apple: ");
+        int pieces;
+        pieces = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Enter total price of " + pieces + " apple(s)");
+    double price;
+    price = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("The total price of " +pieces + " is" +price);
+        Console.WriteLine("The value of original price is " +price);
+
+    double a = price;
+    int b = (int)price;
+Console.WriteLine("The value of the converted price is " +b);
+Console.Write("\nPress any key to exit...");
+Console.ReadKey();
+
+    }
+}
